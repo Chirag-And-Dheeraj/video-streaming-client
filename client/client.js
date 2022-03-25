1de507a1a4a02e880d49edf0b7fe79468527e454
Chirag Lulla <lullachirag239@gmail.coom>
2022-03-25 22:48:45 +0530

    Create video upload form

---
+++ client/client.js
@@ -0,0 +1,5 @@
+const uploadButton = document.querySelector("#upload");
+const fileInput = document.querySelector("input[type=file]");
+uploadButton.addEventListener("click", () => fileInput.click());
+
+
